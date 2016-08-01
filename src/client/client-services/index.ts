86c2eede4558b27275bc422589eb3f5ae90d8486
colinjlacy <colin@webcake.co>
2016-07-31 22:51:34 -0700

    [simple-resolves] wires up some simple resolves to the starter component

---
+++ src/client/client-services/index.ts
@@ -0,0 +1,4 @@
+/**
+ * Created by colinjlacy on 7/31/16.
+ */
+export * from './client-resolves.service';
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,3 +1,4 @@
 export * from './client.component';
 export * from './client-config';
 export * from './internals';
+export * from './client-services/index';
